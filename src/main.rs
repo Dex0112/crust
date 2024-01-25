8271a3740d501d71c417ec1b00eb2b2dcbe8d9c4
Dex0112 <dexcrazy06@gmail.com>
2024-01-24 21:49:16 -0500

    Feat: Implemented parsing tree (not working)

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,70 @@
-struct Node<'a> {
-    value: Token,
-    left: Option<&'a Node<'a>>,
-    right: Option<&'a Node<'a>>,
+struct Node {
+    token: Token,
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>,
 }
 
 enum Token {
     Add,
-    Number(i64),
-    Unrecognized,
+    Subtract,
+    Number(f64),
+    //I am currently unsure of how to detect/handle this
+    //Unrecognized,
     //Assignemnt
     //Sin, Cos, Tan, etc
 }
 
-fn main() {}
+fn main() {
+    let result = evaluate(parse_expression(&"10+5+-7"));
 
-fn parse_expression(expression: &mut str) -> Node {
-    for c in expression.chars() {
-        let token: Token = match c {
-            '+' => Token::Add,
-            _ => {
-                //Is it a number?
-                Token::Unrecognized
-            },
-        };
-    }
-
-    return Node {
-        value: Token::Add,
-        left: None,
-        right: None,
-    };
+    println!("{}", result);
 }
 
-//fn evaluate 
+fn parse_expression(expression: &str) -> Option<Box<Node>> {
+    let is_num = expression.trim().parse::<f64>();
+
+    if is_num.is_ok() {
+        return Some(Box::new(Node {
+            token: Token::Number(is_num.unwrap()),
+            left: None,
+            right: None,
+        }));
+    }
+
+    for (i, c) in expression.char_indices() {
+        let token: Token = match c {
+            '+' => Token::Add,
+            '-' => Token::Subtract,
+            _ => continue,
+        };
+
+        //left = parse_expression(str left of token)
+        let left = parse_expression(&expression[..i].trim());
+        //right = parse_expression(str right of token)
+        let right = parse_expression(&expression[i + 1..].trim());
+
+        return Some(Box::new(Node { token, left, right }));
+    }
+
+    None
+}
+
+fn evaluate(node: Option<Box<Node>>) -> f64 {
+    match node {
+        Some(outer_node) => {
+            let inner_node = *outer_node;
+
+            let left_value = evaluate(inner_node.left);
+            let right_value = evaluate(inner_node.right);
+
+            match inner_node.token {
+                Token::Add => return left_value + right_value,
+                Token::Subtract => return left_value - right_value,
+                Token::Number(num) => return num,
+            }
+
+        },
+
+        None => return 0.0,
+    }
+}
